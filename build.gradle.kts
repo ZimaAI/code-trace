49371f8009442a482dfbf1d21ac956d0cabc71c5
ZimaAI <3300504080@qq.com>
2026-05-29 00:23:09 +0800

    build: bootstrap intellij platform plugin setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,58 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
 plugins {
     id("java")
+    id("org.jetbrains.intellij.platform") version "2.4.0"
 }
 
 group = "com.zimaai.codetrace"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
+    testRuntimeOnly("junit:junit:4.13.2")
+    testRuntimeOnly("org.opentest4j:opentest4j:1.3.0")
+
+    intellijPlatform {
+        intellijIdeaCommunity("2024.3.5")
+        bundledPlugin("com.intellij.java")
+        testFramework(TestFrameworkType.Platform)
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+configurations.configureEach {
+    if (name == "intellijPlatformDependency") {
+        isCanBeConsumed = false
+    }
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "243"
+        }
+    }
 }
